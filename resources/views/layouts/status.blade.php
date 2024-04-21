d0dfa665c9b11e0ed7e9550037ceced76de54a76
devkavin <kavindra.senanayake@gmail.com>
2024-04-21 14:22:02 +0530

    Remove unused code in status.blade.php

--- resources/views/layouts/status.blade.php
+++ resources/views/layouts/status.blade.php
@@ -10,10 +10,6 @@
                 <div class="bg-red-100 border-l-4 border-red-200 text-red-700 p-4 mb-4" role="alert">
                     {{ session('error') }}
                 </div>
-            @elseif (session('warning'))
-                <div class="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mb-4" role="alert">
-                    {{ session('warning') }}
-                </div>
             @endif
         </div>
     </div>
