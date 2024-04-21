9e7b9230855aefc1f9e10e8da0ea28a981b93817
devkavin <kavindra.senanayake@gmail.com>
2024-04-21 06:04:15 +0530

    Remove price column from borrow index view

--- resources/views/borrow/index.blade.php
+++ resources/views/borrow/index.blade.php
@@ -22,7 +22,6 @@
                                 <th class="px-3 py-3">Borrow ID</th>
                                 <th class="px-3 py-3">Borrowed Book Title</th>
                                 <th class="px-3 py-3">Category</th>
-                                <th class="px-3 py-3">Price $</th>
                                 <th class="px-3 py-3">Borrowed At</th>
                                 <th class="px-3 py-3">Returned At</th>
 
@@ -38,7 +37,6 @@
                                             href="{{ route('book.show', $borrow->book->id) }}">{{ $borrow->book->title }}</a>
                                     </td>
                                     <td class="px-3 py-2">{{ $borrow->book->category->name }}</td>
-                                    <td class="px-3 py-2">{{ $borrow->book->price }}</td>
                                     <td class="px-3 py-2">{{ $borrow->borrowed_at }}</td>
                                     <td class="px-3 py-2">{{ $borrow->returned_at ?? 'Not returned' }}</td>
                                     <td class="px-0 py-2 text-center text-nowrap">
